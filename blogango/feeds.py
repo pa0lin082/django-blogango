9857e1d75866ee7b422ce1fea42e356ec1f32097
Paolo Leggio <paolo@digitalmonkeys.it>
2016-03-10 13:06:42 +0100

    fix

--- blogango/feeds.py
+++ blogango/feeds.py
@@ -1,4 +1,5 @@
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
@@ -7,25 +8,49 @@ from blogango.models import Blog, BlogEntry
 from taggit.models import Tag
 
 
+class MyRssFeedGenerator(Rss201rev2Feed):
+
+    def add_item_elements(self, handler, item):
+        super(MyRssFeedGenerator, self).add_item_elements(handler, item)
+        if item.has_key('content:encoded') and item['content:encoded'] is not None:
+            handler.addQuickElement("content:encoded", item['content:encoded'])
+
+
 class main_feed(Feed):
+    feed_type = MyRssFeedGenerator
+
     blog = Blog.objects.get_blog()
     if blog:
         title = blog.title
         link = "/rss/latest/"
         description = blog.tag_line
 
+    def item_extra_kwargs(self, item):
+        """
+        Returns an extra keyword arguments dictionary that is used with
+        the `add_item` call of the feed generator.
+        """
+        content_encoded = item.text_with_abs_url(request=self.request)
+
+        return {
+            # 'content:encoded': content_encoded
+        }
+
     def items(self):
         return BlogEntry.objects.filter(is_page=False)[:10]
 
+    def item_pubdate(self, item):
+        return item.publish_date
+
     def item_description(self, item):
         # print self.request
-
+        # return item.meta_description
         return item.text_with_abs_url(request=self.request)
 
     def get_context_data(self, **kwargs):
         # print 'get_context_data',kwargs
         context = super(main_feed, self).get_context_data(**kwargs)
-        self.request = kwargs.get('request',{})
+        self.request = kwargs.get('request', {})
         return context
 
 class CatFeed(Feed):
